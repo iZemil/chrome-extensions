acbc4553bd81ed4b58398bb0b4d68f54da78c572
zemil <3zemil@gmail.com>
2019-05-05 11:32:38 +0500

    init

---
+++ src/storage.js
@@ -0,0 +1,17 @@
+// storage API: https://developer.chrome.com/extensions/storage
+class Storage {
+    /**
+     * Get object by array of keys
+     * @param {[string]} keys getting keys
+     * @param {function} cb - fn(resulsts) => Object{ key: value, ... }
+     */
+    get(keys = [], cb) {
+        chrome.storage.sync.get(keys, cb);
+    }
+
+    set(data) {
+        chrome.storage.sync.set(data);
+    }
+}
+
+export default new Storage();
